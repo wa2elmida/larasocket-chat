21aa15a5394b66140aa6e4ed91432e5433d8d053
wa2elmida <wa2el2323@gmail.com>
2021-12-16 18:03:12 +0200

    second commet

---
+++ app/Http/Controllers/MessageController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class MessageController extends Controller
+{
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+    public function index()
+    {
+        return Message::with('user')->get();
+    }
+
+    public function store(Request $request)
+    {
+      $user = Auth::user();
+
+       $message = $user->messages()->create([
+           'message' => $request->input('message')
+       ]);    // send event to listeners
+       broadcast(new MessageSentEvent($message, $user))->toOthers();
+
+       return [
+           'message' => $message,
+           'user' => $user,
+       ];
+    }
+}
